25e2f7720a614f0059773d8747f2f46e43c482d2
Visaka <visaka.chaves@consorcio.cl>
2021-09-10 19:17:16 -0300

    fix: membership

--- cride/circles/models/memberships.py
+++ cride/circles/models/memberships.py
@@ -1,4 +1,4 @@
-"""Membership Model"""
+"""Membership model."""
 
 # Django
 from django.db import models
@@ -10,24 +10,26 @@ from cride.utils.models import CRideModel
 class Membership(CRideModel):
     """Membership model.
 
-    A membership is the table that holds
-    relationship between a user and a circle.
-
+    A membership is the table that holds the relationship between
+    a user and a circle.
     """
+
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
     circle = models.ForeignKey('circles.Circle', on_delete=models.CASCADE)
-    is_admin = models.BooleanField('circle admin',
-                                   default=False,
-                                   help_text="Circle admins can update the circle's data and manage its members."
-                                   )
+
+    is_admin = models.BooleanField(
+        'circle admin',
+        default=False,
+        help_text="Circle admins can update the circle's data and manage its members."
+    )
 
     # Invitations
-    used_invitations = models.PositiveBigIntegerField(default=0)
+    used_invitations = models.PositiveSmallIntegerField(default=0)
     remaining_invitation = models.PositiveSmallIntegerField(default=0)
     invited_by = models.ForeignKey(
         'users.User',
-        null=False,
+        null=True,
         on_delete=models.SET_NULL,
         related_name='invited_by'
     )
@@ -40,7 +42,7 @@ class Membership(CRideModel):
     is_active = models.BooleanField(
         'active status',
         default=True,
-        help_text="Only active users are allowed to interact in the circle"
+        help_text='Only active users are allowed to interact in the circle.'
     )
 
     def __str__(self):
