d662d6ebf4bc2a0d82c952663a9a236f08233053
Visaka <visaka.chaves@consorcio.cl>
2021-09-11 18:55:35 -0300

    circle

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,13 +1,14 @@
 """Circle views."""
 
 # Django REST Framework
+from cride.circles import serializers
 from rest_framework import viewsets
 
 # Serializers
 from cride.circles.serializers import CircleModelSerializer
 
 # Models
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
 
 
 class CircleViewSet(viewsets.ModelViewSet):
@@ -15,3 +16,23 @@ class CircleViewSet(viewsets.ModelViewSet):
 
     queryset = Circle.objects.all()
     serializer_class = CircleModelSerializer
+
+    def get_queryset(self):
+        """Restric list to public.only"""
+        queryset = Circle.objects.all()
+        if self.actions == 'list':
+            return queryset.filter(is_public=True)
+        return queryset
+
+    def perform_create(self, serializer):
+        """Assing circle admin"""
+        circle = serializers.save()
+        user = self.request.user
+        profile = user.profile
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True,
+            remaining_invitation=10
+        )
